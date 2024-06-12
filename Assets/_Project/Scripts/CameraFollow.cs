918e00083726e91733d662df50e49e29709ef187
Negar-86 <164731281+Negar-86@users.noreply.github.com>
2024-06-12 13:58:15 +0330

    Update

---
+++ Assets/_Project/Scripts/CameraFollow.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public Transform target;
+    public float smoothSpeed = 0.3f;
+    public Vector3 currentVelocity;
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (target.position.y > transform.position.y)
+        {
+            Vector3 newPos = new Vector3(transform.position.x, target.position.y, transform.position.z);
+            //transform.position = Vector3.SmoothDamp(transform.position, newPos, ref currentVelocity, smoothSpeed * Time.deltaTime);
+            transform.position = newPos;    
+
+        }
+    }
+
+
+}
